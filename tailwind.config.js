684ba6971717267ab486fca8d39626c11772fec8
MeowKawaii <nanthikan.aw@gmail.com>
2023-12-19 15:00:22 +0700

    init style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          light: '#F5F4F9',
+          medium: '#5E3DB3',
+          dark: '#090C35',
+        }
+      }
+    },
   },
   plugins: [],
 }
